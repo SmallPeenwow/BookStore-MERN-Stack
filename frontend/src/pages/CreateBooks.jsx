b3813a51c2007b3ba7ec676065c5fdb2ed13b383
SmallPeenwow <bowdencamryn@gmail.com>
2023-09-04 21:04:33 +0200

    Edit Book In React Done

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -10,6 +10,7 @@ const CreateBooks = () => {
 	const [publishYear, setPublishYear] = useState('');
 	const [loading, setLoading] = useState(false);
 	const navigate = useNavigate();
+
 	const handleSaveBook = () => {
 		const data = {
 			title,
