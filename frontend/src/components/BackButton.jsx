7a84ce498d98a6e7d61ae3e5abb7b7a32291af94
SmallPeenwow <bowdencamryn@gmail.com>
2023-09-06 09:10:01 +0200

    Make Book Card A Single Component Done

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
+import PropTypes from 'prop-types';
 
 const BackButton = ({ destination = '/' }) => {
 	return (
@@ -9,6 +10,10 @@ const BackButton = ({ destination = '/' }) => {
 			</Link>
 		</div>
 	);
+};
+
+BackButton.propTypes = {
+	destination: PropTypes.string,
 };
 
 export default BackButton;
